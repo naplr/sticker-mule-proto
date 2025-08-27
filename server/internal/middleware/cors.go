b6e895a2b3e021161d1465e24ed9e5ace55084dd
Napol Rachatasumrit <napol@cmu.edu>
2025-08-27 17:02:31 +0700

    Fix cors for deployment

--- server/internal/middleware/cors.go
+++ server/internal/middleware/cors.go
@@ -4,7 +4,13 @@ import "net/http"
 
 func CORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		origin := r.Header.Get("Origin")
+
+		// Allow only specific origins
+		if origin == "http://localhost:3000" || origin == "https://mule-fe-1027839195257.us-east4.run.app" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
